3c488219cc4779599b84a951a8843f9da3a104bb
Dalyer <dseiler@sfu.ca>
2018-12-09 12:38:23 -0800

    Updated classes to have basic definitions

--- Category.py
+++ Category.py
@@ -1,3 +1,9 @@
 # Category.py
 
 """The Category Class will have a Name, and a goal or set price for number of monies spent in that place"""
+
+
+class Category:
+
+    def __init__(self, name):
+        self.name = name
--- Transaction.py
+++ Transaction.py
@@ -1,4 +1,21 @@
 # Transaction.py
 """The transaction class will have a category, name, price, date, and transaction type."""
+import os
+import Category
+
+
+class Transaction:
+
+    def __init__(self, name, amount, date, category, type):
+        self.name = name
+        self.amount = amount
+        self.date = date
+        self.category = Category
+        self.type = type
+
+
+
+
+
 
 
