c0682b17c97fa9cf3794d91e9e7fbc57b8cafd79
Dalyer <dseiler@sfu.ca>
2018-12-09 14:23:35 -0800

    Added a very messy parser that creates a list of transactions for debit

--- Main.py
+++ Main.py
@@ -13,6 +13,14 @@ DEBIT_FILE_NAME = 'debit_test.csv'
 CREDIT_FILE_NAME = 'credit_test.csv'
 DATE_RANGE = ''
 
+########HELPER FUNCTIONS##############
+
+def convert_string_list_to_int(list):
+    new_list = []
+    for i in list:
+        new_list.append(int(i))
+    return new_list
+
 # open files
 dirName = os.getcwd()
 
@@ -38,11 +46,39 @@ for line in unsorted_credit_transactions:
     clean_line = [i.strip() for i in line.split(',')]
     sorted_credit_transactions.append(clean_line)
 
+# debit transactions
+# dates are sorted month day year
+sorted_debit_trans_classes = []
+for line in sorted_debit_transactions:
+    # find the proper trans_date and format
+    temp = convert_string_list_to_int([i.strip() for i in line[0].split('/')])
+    trans_date = date(temp[2], temp[0], temp[1])
+    # find the trans_amount
+    trans_category = line[1]  # fix this later so that its a proper category class
+    # find the trans_amount (fix this its terrible)
+    if line[2] != '':
+        trans_amount = float(line[2])
+        trans_type = 'Income'
+    else:
+        trans_amount = float(line[3])
+        trans_type = 'Expense'
+
+    # make transaction and add it to the list
+    sorted_debit_trans_classes.append(Transaction.Transaction(trans_category, trans_amount, trans_date, trans_type))
+
+
+
+
+
+
+
+
+
 
 
 
 #########TESTS#############
-# print(unsorted_debit_transactions[0])
-print(sorted_debit_transactions[0])
-print(sorted_credit_transactions[3])
-
+x = sorted_debit_trans_classes[0]
+print(x)
+print(x.category)
+print(x.date)
